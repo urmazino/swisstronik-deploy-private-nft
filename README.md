766a1ddc5c381feccbaf621ee2ab4f05bfb47e48
urmazino v1 <urekm5131@gmail.com>
2024-07-27 23:35:19 +0000

    first commit

--- README.md
+++ README.md
@@ -132,3 +132,4 @@ twitter : @Izzycracker04
 telegram : @fitriay19
 
 //0x5cED43F3224e3F7C7EFA6ABdE9A960A44E3B2dD9// ignore this
+# swisstronik-deploy-private-nft2
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = '0x5cED43F3224e3F7C7EFA6ABdE9A960A44E3B2dD9'
+const deployedAddress = '0x7Be6Fd23085f77B6586a345f9E4008300e15590b'
 
 export default deployedAddress
